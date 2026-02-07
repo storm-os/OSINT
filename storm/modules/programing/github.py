1c69bd686cffc7ed5a6a50a1ce804196135a8287
Elzy <elzyproot@protonmail.com>
2026-02-08 00:42:05 +0700

    Update github.py

--- storm/modules/programing/github.py
+++ storm/modules/programing/github.py
@@ -1,5 +1,5 @@
-from holehe.core import *
-from holehe.localuseragent import *
+from storm.core import *
+from storm.localuseragent import *
 
 
 async def github(email, client, out):
@@ -8,12 +8,29 @@ async def github(email, client, out):
     method = "register"
     frequent_rate_limit=False
 
-    freq = await client.get("https://github.com/join")
-    token_regex = re.compile(
-        r'<auto-check src="/signup_check/username[\s\S]*?value="([\S]+)"[\s\S]*<auto-check src="/signup_check/email[\s\S]*?value="([\S]+)"')
-    token = re.findall(token_regex, freq.text)
-    data = {"value": email, "authenticity_token": token[0]}
-    req = await client.post("https://github.com/signup_check/email", data=data)
+    try:
+        freq = await client.get("https://github.com/join", headers=headers)
+
+        token_match = re.search(
+            r'<auto-check[^>]*?src="/signup_check/email"[^>]*?>[\s\S]*?value="([^"]+)"', 
+            freq.text
+        )
+        if token_match:
+            auth_token = token_match.group(1)
+            data = {"value": email, "authenticity_token": auth_token}
+            headers["X-Requested-With"] = "XMLHttpRequest"
+        else:
+            raise ValueError("GitHub Email Token not found - Structure changed or Blocked")
+    except Exception:
+        out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                    "rateLimit": True,
+                    "exists": None,
+                    "emailrecovery": None,
+                    "phoneNumber": None,
+                    "others": None})
+        return None
+
+    req = await client.post("https://github.com/signup_check/email", data=data, headers=headers)
     if "Your browser did something unexpected." in req.text:
         out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
                     "rateLimit": True,

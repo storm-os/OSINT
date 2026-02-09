1dc9930116e8d7859e68529d1ed227503eb651e1
Elzy <elzyproot@protonmail.com>
2026-02-10 04:53:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='osint-storm',
-    version="1.61",
+    version="2",
     packages=find_packages(where="."),
     author="megadose (Modified for StormOS)",
     install_requires=[
